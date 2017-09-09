5d6071f47b7a936c8101ba4cb55d44a5d9a8c272
Igor Bondarev <igor.bodnarev@hotmail.com>
2017-09-09 21:07:30 +0300

    Fix realloc

--- vector.c
+++ vector.c
@@ -12,29 +12,25 @@ void sc_vector_init(struct vector *v)
 	v->capacity = DEFAULT_ELEMS_NUM;
 	v->elems = malloc(v->capacity * sizeof(void *));
 
-	printf("Allocated %p size %lu\n", v->elems, v->capacity * sizeof(void *));
-
 	if (v->elems == NULL)
 		sc_utils_die("malloc failed");
 }
 
 void *sc_vector_add(struct vector *v, void *elem)
 {
-	size_t new_len;
-	void *mem;
-
-	if (v->size != v->capacity)
-		return (v->elems[v->size++ - 1] = elem);
+	if (v->size != v->capacity) {
+		v->elems[v->size++] = elem;
+		return v->elems[v->size - 1];
+	}
 		
 	/* Not enought space */
-	new_len = 2 * v->capacity * sizeof(void *);
-	mem  = realloc(v->elems, new_len);
-
-	if (NULL == mem)
+	v->capacity *= 2;
+	if (NULL == (v->elems = realloc(v->elems,
+					v->capacity * sizeof(void *))))
 		sc_utils_die("realloc failed");
-	v->elems = mem;
 
-	return (v->elems[v->size++ - 1] = elem);
+	v->elems[v->size++] = elem;
+	return v->elems[v->size - 1];
 }
 
 void sc_vector_free(const struct vector *v)
